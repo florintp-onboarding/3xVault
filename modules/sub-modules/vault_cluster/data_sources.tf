6be5319ab169dad7c5e851c5579fd17f2b4167d2
Florin Tiucra-Popa <106736683+FlorinTP@users.noreply.github.com>
2024-03-19 13:58:02 +0100

    Added Ubuntu latest LTS

--- modules/sub-modules/vault_cluster/data_sources.tf
+++ modules/sub-modules/vault_cluster/data_sources.tf
@@ -12,7 +12,8 @@ data "aws_ami" "ubuntu" {
     name   = "name"
     #values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
     #values = ["ubuntu/images/hvm-ssd/ubuntu-kinetic-22.10-amd64-server-*"]
-    values = ["ubuntu/images/hvm-ssd/ubuntu-lunar-23.04-amd64-server-*"]
+    #values = ["ubuntu/images/hvm-ssd/ubuntu-lunar-23.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
   filter {
